0b65c3c91f1db3eb638eb2a09228683e79a21455
mohanram <mohanraam2000@gmail.com>
2023-05-28 17:34:33 +0530

    Feature -> Add and Update Features

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -3,6 +3,7 @@ using DapperCRUD.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DapperCRUD.Controllers
 {
@@ -31,7 +32,27 @@ namespace DapperCRUD.Controllers
             var hero = await connection.QueryFirstAsync<SuperHero>($"select * from SuperHeroes where id = @Id", new {Id = HeroId});
             return Ok(hero);
         }
-        
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddSingleHero( SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"    ));
+            await connection.ExecuteAsync("insert into SuperHeroes (Id, Name, FirstName, LastName, Place) values (@Id, @Name, @FirstName, @LastName, @Place)", hero);
+            return Ok(await SelectAllSuperHeroes(connection));
+        }
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllSuperHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("select * from SuperHeroes");
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero Hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update SuperHeroes set Id = @Id, Name = @Name, FirstName = @FirstName, LastName = @LastName, Place = @Place where Id = @Id",Hero);
+            return Ok(await SelectAllSuperHeroes(connection));
+        }
 
         
 
